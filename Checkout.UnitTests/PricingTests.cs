bc56853f6a7bf474cb510f1a963637635de25d70
Aashiq Durga <aashiqdurga@Aashiqs-MBP.iact.co.nz>
2019-03-11 13:49:25 +1300

    Added unit tests for checkout class with pricing for no items

---
+++ Checkout.UnitTests/PricingTests.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class PricingTests
+    {
+        /*
+            assert_equal(  0, price(""))
+    assert_equal( 50, price("A"))
+    assert_equal( 80, price("AB"))
+    assert_equal(115, price("CDBA"))
+        */
+
+        [Test]
+        public void WHEN_Pricing_No_Item_THEN_Total_Is_0()
+        {
+            var checkoutTotalCalculator = new CheckoutTotalCalculator();
+
+            var price = checkoutTotalCalculator.CalculateTotalPrice();
+
+            Assert.AreEqual(0, price);
+        }
+    }
+
+    public class CheckoutTotalCalculator
+    {
+        public decimal CalculateTotalPrice()
+        {
+            return 0;
+        }
+    }
+}
